0182e9c9c6ee24fbdef0ae63fd64e41767f8ba31
C02CK13YMD6M <spencer.woo@liferay.com>
2021-10-09 12:10:45 -0700

    SF - java

--- sources/Java.java
+++ sources/Java.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.text.DecimalFormat;
 
-public class Java {
+public class java {
 
 	public static void main(String[] args) {
 		int range = 0;
@@ -11,46 +11,48 @@ public class Java {
 		try {
 			range = Integer.parseInt(args[0]);
 			trials = Integer.parseInt(args[1]);
+
+			prng(range, trials);
 		} catch (Exception e) {
-			System.out.println("Invalid Arguments");
-			System.out.println("java Java <longegerValue> <longegerValue>");
+			System.err.println("ERROR: Invalid Arguments");
+			System.err.println("Expecting:\n\tjava java <range> <trials>");
 		}
-		
-		prng(range, trials);
 	}
 
-	static void prng(int numbers, int trials) {
-		
-		double frequency[] = new double[numbers];
-		double probability[] = new double[numbers];
+	static void prng(int range, int trials) {
 		long start_time = System.currentTimeMillis();
-		Random random = new Random();
-		String output = "";
+
+		double frequency[] = new double[range];
+		double probability[] = new double[range];
 		
+		Random random = new Random();
 		for (int i = 0; i < trials; i++) {
-			int r_number = random.nextInt(numbers);
+			int r_number = random.nextInt(range);
 			frequency[r_number] += 1;
 		}
-		
-		for (int i = 0; i < numbers; i++) {
+
+		String output = "";
+		for (int i = 0; i < range; i++) {
 			if (frequency[i] == 0) {
-				output += String.format("%d 0\n", i);
+				output += String.format("%d:0\n", i);
 			} else {
 				probability[i] = frequency[i] / trials;
 				DecimalFormat df = new DecimalFormat("#.######");
-				output += String.format("%d : %s\n", i, df.format(probability[i]));
+				output += String.format("%d:%s\n", i, df.format(probability[i]));
 			}
 		}
 		
-		long duration = System.currentTimeMillis() - start_time;	
-		String filename = String.format("%s\\outputs\\java_%s_%s.txt", System.getProperty("user.dir"), numbers, trials);
+		long end_time = System.currentTimeMillis();
+		long duration = end_time - start_time;
 		
 		try {
+			String filename = String.format("%s\\outputs\\java_%s_%s.txt", System.getProperty("user.dir"), range, trials);
+
 			FileWriter fw = new FileWriter(filename);
 			fw.write(output);
 			fw.close();
 		} catch (Exception e) {
-			System.out.println(e);
+			System.err.println(e);
 		}
 		
 		System.out.println(String.format("--- %f seconds ---", (duration * 0.001)));
